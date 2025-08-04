7ffbd66b781e158a059c2af2bab9cfad826d57a8
André Farinha <farinha9b@gmail.com>
2025-08-04 16:00:40 +0100

    g

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ import qs from "querystring";
 dotenv.config();
 
 // ----- ENV -----
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000 || 3000 || 1000;
 const CLIENT_ID = process.env.MOLONI_CLIENT_ID;
 const CLIENT_SECRET = process.env.MOLONI_CLIENT_SECRET;
 const REDIRECT_URI = process.env.MOLONI_CALLBACK_URL;
